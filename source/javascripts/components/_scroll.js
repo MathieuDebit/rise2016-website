d9cd6824d5adbb3cc7ac281ce670db9f10a211a4
Mathieu Débit <mathieu.debit@gmail.com>
2016-02-04 00:09:19 +0100

    Fix shifted scroll to anchors

--- source/javascripts/components/_scroll.js
+++ source/javascripts/components/_scroll.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
       menu = target;
     $target = $(target);
     $('html, body').stop().animate({
-      'scrollTop': $target.offset().top + 2
+      'scrollTop': $target.offset().top - 20
     }, scrollSpeed, 'swing', function() {
       window.location.hash = target;
       $(document).on("scroll", onScroll);
