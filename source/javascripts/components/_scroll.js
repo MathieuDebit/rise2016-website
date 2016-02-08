33afc85c6dae9953fb1057674f9cdc84c6d1e888
valentingalmand <Valentin@MacBook-Pro-de-Valentin-2.local>
2016-02-08 12:16:47 +0100

    Update partial `nav` and responsive it !

--- source/javascripts/components/_scroll.js
+++ source/javascripts/components/_scroll.js
@@ -1,45 +1,10 @@
-// Smooth Scroll
-$(document).ready(function() {
-  $(document).on("scroll", onScroll);
-
-  //smoothscroll
-  var scrollSpeed = 750;
-
-  $('a[href^="#"]').on('click', function(e) {
-    e.preventDefault();
-    $(document).off("scroll");
-    $('a').each(function() {
-      $(this).removeClass('active');
-    });
-    $(this).addClass('active');
-    var target = this.hash,
-      menu = target;
-    $target = $(target);
-    $('html, body').stop().animate({
-      'scrollTop': $target.offset().top - 20
-    }, scrollSpeed, 'swing', function() {
-      window.location.hash = target;
-      $(document).on("scroll", onScroll);
-    });
+//scrollMenu
+$("nav a[href^='#']").on('click', function(e) {
+  e.preventDefault();
+  var hash = this.hash;
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top - 30
+  }, 300, function() {
+    window.location.hash = hash;
   });
-
-  $('#nav .logo').on('click', function(){
-    $('html,body').animate({
-        scrollTop: 0
-    }, scrollSpeed, 'swing');
-  })
 });
-
-function onScroll(event) {
-  var scrollPos = $(document).scrollTop();
-  $('#nav a').each(function() {
-    var currLink = $(this);
-    var refElement = $(currLink.attr("href"));
-    if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-      $('#nav a').removeClass("active");
-      currLink.addClass("active");
-    } else {
-      currLink.removeClass("active");
-    }
-  });
-}
